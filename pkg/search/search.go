a5a19e2b369618ed790ebd980a34820d7611cf3b
Connor Barker <connorlbark@gmail.com>
2020-07-11 11:58:30 -0400

    remove unusued Context struct

--- pkg/search/search.go
+++ pkg/search/search.go
@@ -7,11 +7,6 @@ import (
 	"github.com/porgull/go-search/pkg/environments"
 )
 
-// Context passes in flags to the search algorithm
-type Context struct {
-	Visualize bool
-}
-
 // Result contains statistics about a single run
 // of the search algorithm on an environment
 type Result struct {
